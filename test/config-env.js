f1624c4ae24d33866baa9f6853e3f97443914762
Yen-chi Chen <mark86092@gmail.com>
2019-12-19 23:54:34 +0800

    Fix tests (#646)
    * Fix tests
    
    Ref: https://stackoverflow.com/a/19902459
    
    * Add default value
    * Align the env in travis ci

--- test/config-env.js
+++ test/config-env.js
@@ -2,5 +2,5 @@ process.env.CONTENTFUL_ACCESS_TOKEN = "FakeTokenYouWillFail";
 process.env.CONTENTFUL_SPACE = "rhotsuly6hr2";
 process.env.JWT_SECRET = "DontUseMe";
 process.env.VERIFY_EMAIL_JWT_SECRET = "DontUseMe";
-process.env.MONGODB_URI = "mongodb://localhost";
-process.env.MONGODB_DBNAME = "goodjob-test";
+process.env.MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost";
+process.env.MONGODB_DBNAME = process.env.MONGODB_DBNAME || "goodjob-test";
